aaada50835cdfb36126f2ed7ff4a95bae8167d64
jrg01-lang <jrg01@iesemilidader.com>
2025-11-05 15:59:47 +0100

    ppa

--- Documents/NetBeansProjects/Prueba.T/src/prueba/t/ACT4_2.java
+++ Documents/NetBeansProjects/Prueba.T/src/prueba/t/ACT4_2.java
@@ -15,7 +15,7 @@ public class ACT4_2 {
 
     
     public static void main(String[] args) {
-       int[] array = {1,2,3,4};
+       int[] array = {1,2,3,4,5};
        Scanner scanner = new Scanner(System.in);
        
     }
