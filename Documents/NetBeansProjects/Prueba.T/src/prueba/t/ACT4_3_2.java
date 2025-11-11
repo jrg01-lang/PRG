77060bbc7f54db1b95f8884b0c9706a0908a95f4
jrg01-lang <jrg01@iesemilidader.com>
2025-11-11 18:10:42 +0100

    Nuevo Programa CUC

--- Documents/NetBeansProjects/Prueba.T/src/prueba/t/ACT4_3_2.java
+++ Documents/NetBeansProjects/Prueba.T/src/prueba/t/ACT4_3_2.java
@@ -15,8 +15,6 @@ import prueba.t.UtilitatsConsola;
  */
 public class ACT4_3_2 {
     public static final String ROJO = "\u001B[31m";
-    
-    public static final String RESET = "\u001B[0m";
     public static final String VERDE = "\u001B[32m";
     public static final String AMARILLO = "\u001B[33m";
     public static final String AZUL = "\u001B[34m";
